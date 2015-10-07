594200732346ef59b6766bbbe6cba2a71a0a7005
katherinepamina <katherinelin2016@u.northwestern.edu>
2015-10-07 22:34:45 +0100

    added both right sensors and comments for submission

--- test.m
+++ test.m
@@ -12,27 +12,28 @@ end
 
 while true
     [sensors,av_sensors] = readIRAV(s,av_sensors);
-    % too close to the wall
-    if (av_sensors(5) > high_threshold*1.5)
+    % Obstacle in front
+    if (av_sensors(3) > high_threshold || av_sensors(4) > high_threshold)
+        % Turn left in place
+        speed = [-4,4];
+        turn(s,-4,4);
+    % Too close to the wall
+    elseif (av_sensors(5) > high_threshold*1.5 || av_sensors(6) > high_threshold*1.5)
+        % Turn left curved
         sharpness = calculateTurn(av_sensors(5),high_threshold*1.5,high_threshold*1.5*2);
         if (sum(speed == [sharpness,6] < 1))
             speed = [sharpness,4];
             turn(s,sharpness,4);
         end
-    elseif (av_sensors(3) > high_threshold || av_sensors(4) > high_threshold)
-        % left
-        speed = [-4,4];
-        turn(s,-4,4);
-    
-    % too far away from the wall
-    elseif (av_sensors(6) < low_threshold*1.5)
-        % right
+    % Too far away from the wall
+    elseif (av_sensors(5) < low_threshold*1.5 || av_sensors(6) < low_threshold*1.5)
+        % Turn right curved
         sharpness = calculateTurn(av_sensors(6),0,low_threshold*1.5);
         if (sum(speed == [4,sharpness] < 1))
             speed = [4,sharpness];
             turn(s,4,sharpness);
         end
-    % keep straight
+    % Keep straight
     else
         if (sum(speed == [4,4] < 1))
             speed = [4,4];
@@ -40,35 +41,3 @@ while true
         end
     end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% while true
-%     sensors = readIR(s);
-%     sensors(1);
-%     if (and(sensors(3) < 100,sensors(1) < 90))
-%         turn(s,4,4)
-%     else
-%         if (sensors(1) > 100)
-%             turn(s,4,0)
-%         else
-%             turn(s,0,4)
-%         end
-%     end
-%     
-% end
-% stop(s);
-% fclose(s);
