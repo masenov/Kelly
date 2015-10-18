25894034b4e077644db39136b8a62fa82016bd88
Martin Asenov <s1247380@pescara.inf.ed.ac.uk>
2015-10-18 20:55:30 +0100

    Some more improvements

--- part2_return_to_position.m
+++ part2_return_to_position.m
@@ -19,6 +19,14 @@ while (av_sensors(8) < 300)
     if (counts(1) + counts(2) > 400)
         [x,y,phi,countdown,map] = calculateOdometry(s,counts(1),counts(2),x,y,phi,countdown,map);
         if countdown > global_speed*800
+            turn(s,-4,4);
+            pause(1);
+            counts = readCounts(s);
+            [x,y,phi,countdown,map] = calculateOdometry(s,counts(1),counts(2),x,y,phi,countdown,map);
+            go(s,2);
+            pause(15);
+            counts = readCounts(s);
+            [x,y,phi,countdown,map] = calculateOdometry(s,counts(1),counts(2),x,y,phi,countdown,map);
             break;
         end
     end
@@ -28,16 +36,19 @@ end
 go(s,0);
 readCounts(s);
 direction = rad2deg(atan2(y,x))+180;
-turn(s,-3,3);
-while (abs(phi-direction)>10 && av_sensors(8) < 300)
-    pause(0.05);
+turn(s,-4,4);
+while (abs(phi-direction)>3 && av_sensors(8) < 300)
+    if (abs(phi-direction)<20)
+        turn(s,-1,1);
+    end
+    pause(0.07);
     counts = readCounts(s);
     [x,y,phi,countdown,map] = calculateOdometry(s,counts(1),counts(2),x,y,phi,countdown,map);
     phi
 end
 go(s,2);
-while (abs(x) + abs(y) > 5)
-    pause(0.05);
+while (abs(x) + abs(y) > 2)
+    pause(0.07);
     counts = readCounts(s);
     [x,y,phi,countdown,map] = calculateOdometry(s,counts(1),counts(2),x,y,phi,countdown,map);
 end
