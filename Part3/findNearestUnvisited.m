126ccca652e1cdf7d3f347112d3a55c0ee326b3b
katherinepamina <katherinelin2016@u.northwestern.edu>
2015-11-09 18:18:05 +0000

    finding nearest unvisited cell

--- Part3/findNearestUnvisited.m
+++ Part3/findNearestUnvisited.m
@@ -1,6 +1,16 @@
-function unvisited = findNearestUnvisited( curr )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function cell = findNearestWithValue( curr, map, value )
+% returns the unvisited cell on the map nearest to current cell (when value is set to unvisited)
+% CONSIDER:would it be better to return the path to this cell? avoid doing this calculation twice
+
+s = size(map);
+mapboundx = s(2); %col currently 20 20
+mapboundy = s(1); %row currently 12
+
+%look for the nearest cell of value value
+
+if map(curr(1), curr(2) == value)
+return curr;
+end
 
 
 end
